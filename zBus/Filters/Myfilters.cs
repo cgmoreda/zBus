22a3a793826fc50eb8870afa6fa34bc19db19ff2
Sherif Elglaly <sherifelglaly@gmail.com>
2024-05-18 10:46:14 +0300

    Update Account Admin

---
+++ zBus/Filters/Myfilters.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+
+namespace zBus.Filters
+{
+    public class RoleAuthorizationFilter : IAuthorizationFilter
+    {
+        private readonly string _role;
+
+        public RoleAuthorizationFilter(string role)
+        {
+            _role = role;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var userEmail = context.HttpContext.Session.GetString("UserRole");
+            if (string.IsNullOrEmpty(userEmail))
+            {
+                context.Result = new RedirectToActionResult("LoginReminder", "Shared", null);
+                return;
+            }
+        }
+    }
+
+    public class LoginAuthorizationFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var userEmail = context.HttpContext.Session.GetString("UserEmail");
+            if (string.IsNullOrEmpty(userEmail))
+            {
+                context.Result = new RedirectToActionResult("LoginReminder", "Shared", null);
+            }
+        }
+    }
+}
